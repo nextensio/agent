d45ebbe519f02f9c360f124b8a8678173c7fddee
Gopa Kumar <gopakumarce@gmail.com>
2021-03-15 18:17:26 -0400

    remove Go android version, thats just not work-able

--- go/platforms/android/app/tools/dummy/dummy.c
+++
@@ -1,7 +0,0 @@
-// In CMakeLists.txt unless we do an add_dependencies(libdummy.so libnxt-go.so),
-// the darn thing doesnt compile and craps out saying some command_logs.json not
-// generated etc.. So just to get a dependency added, here is a dummy lib
-int main(int argc, char *argv[])
-{
-    return 0;
-}
