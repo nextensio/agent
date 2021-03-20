ae1fbaed6e6defdd93c107835dc7594547086edd
Rudy Zulkarnain <rzulkarn@yahoo.com>
2021-03-20 09:00:18 -0700

    add rust build for apple

--- rust/agent/build_apple.sh
+++ rust/agent/build_apple.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-cargo lipo --release
+# cargo lipo --release
+cargo build --target aarch64-apple-ios --release
+cargo build --target x86_64-apple-ios --release
+cargo build --target x86_64-apple-darwin --release
+\rm -rf ../target/libnextensioIOS.a
+\rm -rf ../target/libnextensioMACOSX.a
+lipo -create ../target/aarch64-apple-ios/release/libnextensio.a ../target/x86_64-apple-ios/release/libnextensio.a -output ../target/libnextensioIOS.a
+lipo -create ../target/x86_64-apple-darwin/release/libnextensio.a -output ../target/libnextensioMACOSX.a
+lipo -info ../target/libnextensioIOS.a
+lipo -info ../target/libnextensioMACOSX.a
