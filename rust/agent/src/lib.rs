17e6595361cf40e800a0fd92ceab4f864c0e5188
Gopa Kumar <gopakumarce@gmail.com>
2021-06-18 11:43:41 -0400

    debugs

--- rust/agent/src/lib.rs
+++ rust/agent/src/lib.rs
@@ -1623,7 +1623,7 @@ fn monitor_gw(agent: &mut AgentInfo, poll: &mut Poll) {
             STATS_GWUP.store(0, Relaxed);
             STATS_NUMFLAPS.fetch_add(1, Relaxed);
             agent.last_flap = Instant::now();
-            error!("Gateway transport closed, try opening again");
+            error!("GOPA: Gateway transport closed, try opening again");
             agent.gw_onboarded = false;
             gw_tun
                 .tun
