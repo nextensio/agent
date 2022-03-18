cdbdb4e710a39b1ceb8721335b68629614082d6e
Gopa Kumar <gopakumarce@gmail.com>
2022-03-18 09:49:42 -0400

    headroom and prepend tests added

--- rust/agent/src/test.rs
+++ rust/agent/src/test.rs
@@ -1,10 +1,19 @@
 use super::*;
 
-fn pkt_parse(headroom: usize, bytes: usize, pendsz: usize, outlen: Vec<usize>) {
+fn pkt_parse(headroom: usize, pendstart: usize, bytes: usize, pendsz: usize, outlen: Vec<usize>) {
     let mut agent = AgentInfo::default();
     agent_init_pools(&mut agent, 1500, 0);
     let mut pending = pool_get(agent.ext.pkt_pool.clone()).unwrap();
     pending.clear();
+
+    if pendstart != 0 {
+        let mut prepend = "".to_string();
+        for _ in 0..pendstart {
+            prepend.push_str("\n");
+        }
+        let prepend_u8 = prepend.as_bytes();
+        pending.extend_from_slice(prepend_u8);
+    }
 
     let mut http = r#"GET /hello.htm HTTP/1.1
 User-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)
@@ -82,51 +91,75 @@ X-Long-Header: "#
 #[test]
 fn pkt_parse_zero_bufs() {
     let v = vec![];
-    pkt_parse(0, 1024, 1230, v);
+    pkt_parse(0, 0, 1024, 1230, v);
 }
 
 #[test]
 fn pkt_parse_one_bufs() {
     let v = vec![206];
-    pkt_parse(0, 2 * 1024, 2048, v);
+    pkt_parse(0, 0, 2 * 1024, 2048, v);
 }
 
 #[test]
 fn pkt_parse_two_bufs() {
     let v = vec![63488, 206];
-    pkt_parse(0, 64 * 1024, 2048, v);
+    pkt_parse(0, 0, 64 * 1024, 2048, v);
 }
 
 #[test]
 fn pkt_parse_three_bufs() {
     let v = vec![63488, 64 * 1024, 206];
-    pkt_parse(0, 2 * 64 * 1024, 2048, v);
+    pkt_parse(0, 0, 2 * 64 * 1024, 2048, v);
 }
 
 #[test]
 fn pkt_parse_zero_bufs_headroom() {
     const HEADROOM: usize = 256;
     let v = vec![];
-    pkt_parse(HEADROOM, 1024, 1230, v);
+    pkt_parse(HEADROOM, 0, 1024, 1230, v);
 }
 
 #[test]
 fn pkt_parse_one_bufs_headroom() {
     const HEADROOM: usize = 256;
     let v = vec![206];
-    pkt_parse(HEADROOM, 2 * 1024, 2048, v);
+    pkt_parse(HEADROOM, 0, 2 * 1024, 2048, v);
 }
 
 #[test]
 fn pkt_parse_two_bufs_headroom() {
     const HEADROOM: usize = 256;
     let v = vec![63488 - HEADROOM, 206 + HEADROOM];
-    pkt_parse(HEADROOM, 64 * 1024, 2048, v);
+    pkt_parse(HEADROOM, 0, 64 * 1024, 2048, v);
 }
 
 #[test]
 fn pkt_parse_three_bufs_headroom() {
     const HEADROOM: usize = 256;
     let v = vec![63488 - HEADROOM, 64 * 1024, 206 + HEADROOM];
-    pkt_parse(HEADROOM, 2 * 64 * 1024, 2048, v);
+    pkt_parse(HEADROOM, 0, 2 * 64 * 1024, 2048, v);
+}
+
+#[test]
+fn pkt_parse_zero_bufs_headroom_prepend() {
+    const HEADROOM: usize = 256;
+    const PREPEND: usize = 50;
+    let v = vec![];
+    pkt_parse(HEADROOM, PREPEND, 1024, 1230 + PREPEND, v);
+}
+
+#[test]
+fn pkt_parse_one_bufs_headroom_prepend() {
+    const HEADROOM: usize = 256;
+    const PREPEND: usize = 50;
+    let v = vec![206 + PREPEND];
+    pkt_parse(HEADROOM, PREPEND, 2 * 1024, 2048, v);
+}
+
+#[test]
+fn pkt_parse_three_bufs_headroom_prepend() {
+    const HEADROOM: usize = 256;
+    const PREPEND: usize = 50;
+    let v = vec![63488 - HEADROOM + PREPEND, 64 * 1024, 206 + HEADROOM];
+    pkt_parse(HEADROOM, PREPEND, 2 * 64 * 1024, 2048, v);
 }
